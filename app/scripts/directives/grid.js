57e2666c73a3920503f092bade2fb141db34ca96
Albert-Cyberhulk <albstepanyan@gmail.com>
2015-08-27 22:39:47 +0200

    Adding styles

--- app/scripts/directives/grid.js
+++ app/scripts/directives/grid.js
@@ -21,7 +21,7 @@ app.directives.directive("grid", ['$log', '$compile',
       compile: function(cElem, cAttrs) {
         //linking function
         return function(lscope, lElem, lAttrs) {
-          lscope.$watch("gridData" , function(n,o){ 
+          lscope.$watch("gridData" , function(n,o){
             lElem.html(JSON.stringify(lscope.gridData));
             $compile(lElem.contents())(lscope);
           });
